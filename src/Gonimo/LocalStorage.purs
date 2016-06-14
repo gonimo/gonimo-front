cf1b6bc76c6bfd44892f15238c5ed861d1da0b02
Robert Klotzner <robert.klotzner@gmx.at>
2016-06-14 17:30:24 +0200

    WIP: Make it compile again

--- src/Gonimo/LocalStorage.purs
+++ src/Gonimo/LocalStorage.purs
@@ -4,10 +4,3 @@ import Browser.LocalStorage
 import Data.Generic
 import Data.Tuple
 import Gonimo.Server.Types
-
-data Key a = MachineCredentials
-
-derive instance genericStorageData :: Generic (Key a)
-
-machineCredentials :: Key MachineCredentials
-machineCredentials = MachineCredentials
--- src/Gonimo/Server/Types.purs
+++ src/Gonimo/Server/Types.purs
@@ -24,11 +24,16 @@ type ServerT e = ReaderT Config (Aff (ajax :: AJAX | e))
 type ServerRequest = AffjaxRequest Json
 
 
-type MachineCredentials = Tuple AccountId AuthToken
-
 newtype Date = DateTime String
+
+derive instance genericDate :: Generic Date
+
 newtype Secret = Secret String
+
+derive instance genericSecret :: Generic Secret
 
 data Key a = Int
 
-derive instance genericKey :: Generic Key
+derive instance genericKey :: Generic (Key a)
+
+data Family = Family -- Dummy type for Key Family
