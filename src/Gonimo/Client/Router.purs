ec19a63be30974d30798356844652f967cd3bb40
Robert Klotzner <robert.klotzner@gmx.at>
2016-09-13 13:50:11 +0200

    Fixed Router - regex to the rescue
    For one reason or another pux routing stopped working for me again.
    
    Now I am simply using a good old regex and it is working again! :-)

--- src/Gonimo/Client/Router.purs
+++ src/Gonimo/Client/Router.purs
@@ -14,6 +14,14 @@ import Partial.Unsafe (unsafeCrashWith)
 import Prelude (($), (<$>))
 import Pux.Router (param, router, lit, int, end)
 import Servant.PureScript.Settings (gDefaultEncodeURLPiece)
+import Debug.Trace (trace)
+import Data.Maybe (Maybe(..))
+import Data.Array (index)
+import Data.Either (fromRight)
+import Partial.Unsafe (unsafePartial)
+
+import Data.String.Regex as R
+
 
 data Route = Home | AcceptInvitation Secret | DecodingFailed String
 
@@ -23,17 +31,27 @@ render (AcceptInvitation secret) = "acceptInvitation=" <> gDefaultEncodeURLPiece
 render Home = ""
 
 match' :: String -> RouteP
-match' url = fromMaybe (DecodingFailedP $ "No such route: " <> url) $ router url $
-       AcceptInvitationP <$> (lit "index.html" *> param "acceptInvitation") <* end
-   <|> HomeP <$ lit "index.html" <* end
-
+-- match' url =trace ("Routing: " <> url) $ \_ -> (fromMaybe (DecodingFailedP $ "No such route: " <> url) $ router url $
+--        AcceptInvitationP <$> (lit "index.html" *> param "acceptInvitation") <* end
+--    <|> HomeP <$ lit "index.html" <* end
+-- )
+match' url = let
+    matcher = unsafePartial $ fromRight $ R.regex ".*acceptInvitation=([^&]*)" R.noFlags
+    matched = R.match matcher url
+  in
+     case matched of
+       Nothing -> HomeP
+       Just arr -> case index arr 1 of
+         Nothing -> HomeP
+         (Just Nothing) -> HomeP
+         (Just (Just val)) -> AcceptInvitationP val
 
 match :: String -> Route
 match url = let r' = match' url
             in case r' of
-              HomeP -> Home
-              DecodingFailedP err -> DecodingFailed err
-              AcceptInvitationP str ->
+              HomeP -> trace ("Routing: " <> url <> ", to Home") $ \_ -> Home
+              DecodingFailedP err -> trace ("Routing:" <> url <> ", to DecodingFailed") $ \_ -> DecodingFailed err
+              AcceptInvitationP str -> trace ("Routing:" <> url <> ", to AcceptInvitationP, match: " <> str) $ \_ ->
                 let eVal = Aeson.decodeJson =<< (jsonParser <<< decodeURIComponent) str
                 in case eVal of
                   Left err  -> DecodingFailed err
