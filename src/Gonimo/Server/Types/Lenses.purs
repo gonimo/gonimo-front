e1fa05b6ccacfa88bea2688dabd2a99214b47930
Robert Klotzner <robert.klotzner@gmx.at>
2016-11-03 08:53:39 +0100

    Added lenses for server types.

---
+++ src/Gonimo/Server/Types/Lenses.purs
@@ -0,0 +1,44 @@
+module Gonimo.Server.Types.Lenses where
+
+import Gonimo.Server.Types
+import Data.Either as Either
+import Data.Lens as Lens
+import Prelude as Prelude
+import Gonimo.Types (Secret(Secret))
+
+
+_GonimoSecret :: Lens.PrismP AuthToken Secret
+_GonimoSecret = Lens.prism GonimoSecret unwrap
+  where
+    unwrap (GonimoSecret x) = Either.Right x
+    unwrap y = Either.Left y
+
+_Tea :: Lens.PrismP Coffee Prelude.Unit
+_Tea = Lens.prism (Prelude.const Tea) unwrap
+  where
+    unwrap Tea = Either.Right Prelude.unit
+    unwrap y = Either.Left y
+
+_EmailInvitation :: Lens.PrismP InvitationDelivery String
+_EmailInvitation = Lens.prism EmailInvitation unwrap
+  where
+    unwrap (EmailInvitation x) = Either.Right x
+    unwrap y = Either.Left y
+
+_OtherDelivery :: Lens.PrismP InvitationDelivery Prelude.Unit
+_OtherDelivery = Lens.prism (Prelude.const OtherDelivery) unwrap
+  where
+    unwrap OtherDelivery = Either.Right Prelude.unit
+    unwrap y = Either.Left y
+
+_NoBaby :: Lens.PrismP DeviceType Prelude.Unit
+_NoBaby = Lens.prism (Prelude.const NoBaby) unwrap
+  where
+    unwrap NoBaby = Either.Right Prelude.unit
+    unwrap y = Either.Left y
+
+_Baby :: Lens.PrismP DeviceType String
+_Baby = Lens.prism Baby unwrap
+  where
+    unwrap (Baby x) = Either.Right x
+    unwrap y = Either.Left y
