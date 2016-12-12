7b097a4a4b98a6d199779ed312959a684c6996a9
Martin Heuschober <epsilonhalbe@gmail.com>
2016-12-12 23:19:20 +0100

    browserify, uglify and `--prod` flag
    - app.js is still built in support

--- build/Main.hs
+++ build/Main.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 module Main where
 
 import           Control.Monad
+import           System.Console.GetOpt
+import           Control.Category ((>>>))
 import qualified Data.ByteString.Lazy.Char8  as B
+import           Data.ByteString.Lazy.Char8  (ByteString)
 import qualified Data.ByteString.Char8  as B'
 {-import           Data.ByteString.Lazy.Char8  (ByteString)-}
 import           Data.ByteString.Lazy.Search
@@ -20,28 +23,42 @@ import           System.FilePath
 data Located a = Located FilePath a
   deriving (Eq, Ord, Show)
 
+data Flags = Prod
+  deriving (Eq)
+
+flags :: forall a. [OptDescr (Either a Flags)]
+flags = [Option "" ["prod"] (NoArg $ Right Prod) "build for production"]
+
 main :: IO ()
-main = shakeArgs shakeOptions build
+main = shakeArgsWith shakeOptions flags $ \flagVals _ -> build (Prod `elem` flagVals)
 
 srcDir, outputDir :: FilePath
 srcDir = "support"
 outputDir = "dist"
+
 templateName, templateName' :: FilePath
 templateName = "prod.index.html"
 templateName' = srcDir </> templateName
+
 outputName, outputName' :: FilePath
 outputName = "index.html"
 outputName' = outputDir </> outputName
+
 appName, appName' :: FilePath
 appName = "app.js"
 appName' = srcDir </> appName
 
-build :: Rules ()
-build = do
-  want [srcDir</> "app.js"]
-  action $ do psFiles <- getDirectoryFiles "src" ["//*"]
-              need psFiles
-              unit $ cmd browserify appName'
+build :: Bool -> IO (Maybe (Rules ()))
+build isProd = return $ Just $ do
+  want [appName']
+  action $ do
+    psFiles <- getDirectoryFiles "" ["src//*"]
+    need (psFiles ++ ["bower.json", "package.json"])
+    unit (bowerInstall >> bowerPrune)
+    unit npmInstall
+    fmap sed browserify >>= (if isProd then uglify else return)
+                        >>= (liftIO . B.writeFile appName')
+
   want $ map ("static" </>) [outputName']
   action $ do
     support <- getDirectoryFiles "support" ["//*"]
@@ -58,15 +75,23 @@ build = do
     let (indexHTML,filesToCopy) = M.foldrWithKey replaceCollect (template,[]) supportMD5
 
     for_ filesToCopy $ \f -> copyFile' (srcDir </> f) ("static" </> outputDir </> f)
-    liftIO $ print supportMD5
-    liftIO $ B.writeFile ("static"</>outputName') indexHTML
+    liftIO $ B.writeFile ("static" </> outputName') indexHTML
 
  where replaceCollect str rep tl@(tmplt,l) =
          if null $ indices str tmplt
             then tl
-            else (replace ("/"<>str) rep tmplt, B'.unpack str:l)
-       browserify :: String
-       browserify = "pulp browserify --to"
+            else (replace (B'.pack "/"<>str) rep tmplt, B'.unpack str:l)
+
+       browserify :: Action ByteString
+       browserify = fromStdout <$> cmd "pulp" ("browserify" : ["-O" | isProd])
+       sed :: ByteString -> ByteString
+       sed = replace (B'.pack "://localhost:8081/") (B.pack "s://b00.gonimo.com/")
+       uglify :: ByteString -> Action ByteString
+       uglify s = fromStdout <$> cmd (AddPath ["./node_modules/uglify-js/bin/"] []) "uglifyjs" ["-c","--screw-ie8","-m"] (StdinBS s)
+       npmInstall, bowerInstall, bowerPrune :: Action ()
+       npmInstall = cmd "npm" ["install"]
+       bowerInstall = cmd "bower" ["install"]
+       bowerPrune = cmd "bower" ["prune"]
 
 removeTLD :: FilePath -> FilePath
 removeTLD = joinPath . drop 1 . splitPath
